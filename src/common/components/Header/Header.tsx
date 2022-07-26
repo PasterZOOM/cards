ac700576da49858b86084302b835a21c36e42a7d
Ilya Pasternak <pasterzoom@gmail.com>
2022-07-26 12:37:31 +0300

    change: BrowserRouter fix: Header styles

--- src/common/components/Header/Header.tsx
+++ src/common/components/Header/Header.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 
+import AppBar from '@mui/material/AppBar/AppBar';
 import Button from '@mui/material/Button/Button';
+import Grid from '@mui/material/Grid/Grid';
+import Toolbar from '@mui/material/Toolbar/Toolbar';
 import { NavLink } from 'react-router-dom';
 
 import styles from './Header.module.css';
@@ -11,15 +14,25 @@ import { ReturnComponentType } from 'types/ReturnComponentType';
 
 export const Header = (): ReturnComponentType => {
   return (
-    <div className={styles.main}>
-      <NavLink to={path.PROFILE} className={styles.helperItem}>
-        <img src={logo} alt="logo" className={styles.logo} />
-      </NavLink>
-      <NavLink to={path.LOGIN} className={styles.helperItem}>
-        <Button variant="contained" color="primary" className={styles.button}>
-          <span className={styles.buttonText}>Sign in</span>
-        </Button>
-      </NavLink>
-    </div>
+    <AppBar position="static" className={styles.main}>
+      <Toolbar className={styles.toolbar}>
+        <Grid container justifyContent="space-between" alignItems="center">
+          <Grid item>
+            <NavLink to={path.LOGIN}>
+              <Button aria-controls="menu-appbar" aria-haspopup="true">
+                <img src={logo} alt="logo" className={styles.logo} />
+              </Button>
+            </NavLink>
+          </Grid>
+          <Grid item>
+            <NavLink to={path.LOGIN} className={styles.link}>
+              <Button variant="contained" color="primary" className={styles.button}>
+                Sign in
+              </Button>
+            </NavLink>
+          </Grid>
+        </Grid>
+      </Toolbar>
+    </AppBar>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { Provider } from 'react-redux/es/exports';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import reportWebVitals from './reportWebVitals';
 
@@ -13,11 +13,11 @@ import { store } from 'app/store';
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
-  <HashRouter>
+  <BrowserRouter>
     <Provider store={store}>
       <App />
     </Provider>
-  </HashRouter>,
+  </BrowserRouter>,
 );
 
 reportWebVitals();
